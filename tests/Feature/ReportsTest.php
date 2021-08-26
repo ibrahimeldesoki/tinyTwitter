3d648ce8a64f3525f3a361cbfa8df1e67190753a
ibrahimeldesoki <ibrahimeldesoki@users.noreply.github.com>
2021-08-26 11:25:49 +0000

    Apply fixes from StyleCI

--- tests/Feature/ReportsTest.php
+++ tests/Feature/ReportsTest.php
@@ -17,11 +17,11 @@ class ReportsTest extends TestCase
     public function test_user_reports_is_downloadable()
     {
         $user = factory(User::class)->create();
-        factory(Tweet::class ,30)->create(['user_id'=>$user->id]);
-        $token= JWTAuth::fromUser($user);
+        factory(Tweet::class, 30)->create(['user_id'=>$user->id]);
+        $token = JWTAuth::fromUser($user);
 
-        $this->get('/api/v1/report',['Authorization' => 'Bearer'.$token])
+        $this->get('/api/v1/report', ['Authorization' => 'Bearer'.$token])
         ->assertStatus(200)
-        ->assertHeader('content-type','application/pdf');
+        ->assertHeader('content-type', 'application/pdf');
     }
 }
